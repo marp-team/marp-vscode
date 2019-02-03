1d6d4ab5fc239347e7c89349b5c35b9347bcd9b7
Yuki Hattori <yukihattori1116@gmail.com>
2019-02-03 22:52:46 +0900

    Refactor extension.ts to be simplify

--- src/extension.ts
+++ src/extension.ts
@@ -1,56 +1,50 @@
 import { Marp } from '@marp-team/marp-core'
-import { ExtensionContext } from 'vscode'
 
 const frontMatterRegex = /^---\s*([^]*)?(?:-{3}|\.{3})\s*/
 const marpDirectiveRegex = /^marp\s*:\s*true\s*$/m
 const twemojiCDNRegex = /\bhttps?:\/\/twemoji\.maxcdn\.com\/(2\/)?svg\/([0-9a-z-]+)\.svg\b/i
 
-export function activate(context: ExtensionContext) {
-  return {
-    extendMarkdownIt(md: any) {
-      const marp: any = new Marp({
-        container: { tag: 'div', id: 'marp-vscode' },
-      })
+function extendMarkdownIt(md: any) {
+  const marp: any = new Marp({
+    container: { tag: 'div', id: 'marp-vscode' },
+  })
 
-      md.use(marp.markdownItPlugins).use(instance => {
-        instance.core.ruler.before('normalize', 'marp_vscode_toggle', state => {
-          const fmMatch = frontMatterRegex.exec(state.src)
+  md.use(marp.markdownItPlugins).use(instance => {
+    instance.core.ruler.before('normalize', 'marp_vscode_toggle', state => {
+      const fmMatch = frontMatterRegex.exec(state.src)
+      state.marpit(!!(fmMatch && marpDirectiveRegex.exec(fmMatch[1].trim())))
+    })
+  })
 
-          state.marpit(
-            !!(fmMatch && marpDirectiveRegex.exec(fmMatch[1].trim()))
-          )
-        })
-      })
+  marp.markdown = md
+  marp.use(instance => {
+    instance.core.ruler.push('marp_vscode_style', ({ Token, tokens }) => {
+      const css = marp.renderStyle(marp.lastGlobalDirectives.theme)
+      const token = new Token('marp_vscode_style', '', 0)
 
-      marp.markdown = md
-      marp.use(instance => {
-        instance.core.ruler.push('marp_vscode_style', ({ Token, tokens }) => {
-          const css = marp.renderStyle(marp.lastGlobalDirectives.theme)
-          const token = new Token('marp_vscode_style', '', 0)
+      token.content = css
+      tokens.unshift(token)
+    })
+  })
 
-          token.content = css
-          tokens.unshift(token)
-        })
-      })
+  md.renderer.rules.marp_vscode_style = (token, i) =>
+    `<style id="marp-vscode-style">${token[i].content}</style>`
 
-      md.renderer.rules.marp_vscode_style = (token, i) =>
-        `<style id="marp-vscode-style">${token[i].content}</style>`
-
-      // VSCode cannot render the external SVG resource by security reason.
-      const { emoji, marp_unicode_emoji: unicodeEmoji } = md.renderer.rules
-      const replaceTwemojiSVG = (base: string) => {
-        return base.replace(
-          twemojiCDNRegex,
-          (_, ver, code) =>
-            `https://twemoji.maxcdn.com/${ver || ''}72x72/${code}.png`
-        )
-      }
-
-      md.renderer.rules.emoji = (...args) => replaceTwemojiSVG(emoji(...args))
-      md.renderer.rules.marp_unicode_emoji = (...args) =>
-        replaceTwemojiSVG(unicodeEmoji(...args))
-
-      return md
-    },
+  // VSCode cannot render the external SVG resource by security reason.
+  const { emoji, marp_unicode_emoji: unicodeEmoji } = md.renderer.rules
+  const replaceTwemojiSVG = (base: string) => {
+    return base.replace(
+      twemojiCDNRegex,
+      (_, ver, code) =>
+        `https://twemoji.maxcdn.com/${ver || ''}72x72/${code}.png`
+    )
   }
+
+  md.renderer.rules.emoji = (...args) => replaceTwemojiSVG(emoji(...args))
+  md.renderer.rules.marp_unicode_emoji = (...args) =>
+    replaceTwemojiSVG(unicodeEmoji(...args))
+
+  return md
 }
+
+export const activate = () => ({ extendMarkdownIt })
