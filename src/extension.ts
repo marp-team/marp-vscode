d4d7f5b3d13de22ec0c6d48d4bb1198b62b1fadf
Yuki Hattori <yuki.hattori@speee.jp>
2019-02-04 18:18:41 +0900

    Update Marp Core and use twemoji.ext option

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,11 @@ import { Marp } from '@marp-team/marp-core'
 
 const frontMatterRegex = /^---\s*([^]*)?(?:-{3}|\.{3})\s*/
 const marpDirectiveRegex = /^marp\s*:\s*true\s*$/m
-const twemojiCDNRegex = /\bhttps?:\/\/twemoji\.maxcdn\.com\/(2\/)?svg\/([0-9a-z-]+)\.svg\b/i
 
 function extendMarkdownIt(md: any) {
   const marp: any = new Marp({
     container: { tag: 'div', id: 'marp-vscode' },
+    emoji: { twemoji: { ext: "png" } },
   })
 
   md.use(marp.markdownItPlugins).use(instance => {
@@ -29,20 +29,6 @@ function extendMarkdownIt(md: any) {
 
   md.renderer.rules.marp_vscode_style = (token, i) =>
     `<style id="marp-vscode-style">${token[i].content}</style>`
-
-  // VSCode cannot render the external SVG resource by security reason.
-  const { emoji, marp_unicode_emoji: unicodeEmoji } = md.renderer.rules
-  const replaceTwemojiSVG = (base: string) => {
-    return base.replace(
-      twemojiCDNRegex,
-      (_, ver, code) =>
-        `https://twemoji.maxcdn.com/${ver || ''}72x72/${code}.png`
-    )
-  }
-
-  md.renderer.rules.emoji = (...args) => replaceTwemojiSVG(emoji(...args))
-  md.renderer.rules.marp_unicode_emoji = (...args) =>
-    replaceTwemojiSVG(unicodeEmoji(...args))
 
   return md
 }
