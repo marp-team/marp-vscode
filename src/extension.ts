818dd6928d79173c9abb452a556949e710376e8c
Yuki Hattori <yukihattori1116@gmail.com>
2019-02-05 23:45:55 +0900

    Improve Marp rendering together default style and famous plugins

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import { Marp } from '@marp-team/marp-core'
 
 const frontMatterRegex = /^---\s*([^]*)?(?:-{3}|\.{3})\s*/
 const marpDirectiveRegex = /^marp\s*:\s*true\s*$/m
+const marpVscodeEnabled = Symbol()
 
 function extendMarkdownIt(md: any) {
   const marp: any = new Marp({
@@ -9,30 +10,74 @@ function extendMarkdownIt(md: any) {
     emoji: { twemoji: { ext: 'png' } },
   })
 
-  md.use(marp.markdownItPlugins).use(instance => {
-    instance.core.ruler.before('normalize', 'marp_vscode_toggle', state => {
-      if (state.inlineMode) return
+  md.use(marp.markdownItPlugins)
+    .use(instance => {
+      // Detect `marp: true` front-matter option
+      instance.core.ruler.before('normalize', 'marp_vscode_toggle', state => {
+        if (state.inlineMode) return
 
-      const fmMatch = frontMatterRegex.exec(state.src)
-      state.marpit(!!(fmMatch && marpDirectiveRegex.exec(fmMatch[1].trim())))
+        const fmMatch = frontMatterRegex.exec(state.src)
+        const enabled = !!(
+          fmMatch && marpDirectiveRegex.exec(fmMatch[1].trim())
+        )
+
+        instance[marpVscodeEnabled] = enabled
+        state.marpit(enabled)
+      })
     })
-  })
+    .use(instance => {
+      let originalEmojiRule
 
+      instance.core.ruler.push('marp_vscode_postprocess', state => {
+        if (state.inlineMode) return
+
+        // Override markdown-it-emoji renderer provided by other plugin
+        if (
+          !originalEmojiRule &&
+          Object.prototype.hasOwnProperty.call(instance.renderer.rules, 'emoji')
+        ) {
+          originalEmojiRule = instance.renderer.rules.emoji
+          instance.renderer.rules.emoji = function(...args) {
+            if (instance[marpVscodeEnabled]) {
+              return instance.renderer.rules.marp_emoji(...args)
+            }
+            return originalEmojiRule(...args)
+          }
+        }
+      })
+    })
+
+  // Render converted CSS together with Markdown
   marp.markdown = md
   marp.use(instance => {
-    instance.core.ruler.push('marp_vscode_style', ({ Token, tokens, inlineMode }) => {
-      if (inlineMode) return
+    instance.core.ruler.push(
+      'marp_vscode_style',
+      ({ Token, tokens, inlineMode }) => {
+        if (inlineMode) return
 
-      const css = marp.renderStyle(marp.lastGlobalDirectives.theme)
-      const token = new Token('marp_vscode_style', '', 0)
+        const css = marp.renderStyle(marp.lastGlobalDirectives.theme)
+        const token = new Token('marp_vscode_style', '', 0)
 
-      token.content = css
-      tokens.unshift(token)
-    })
+        token.content = css
+        tokens.unshift(token)
+      }
+    )
   })
 
   md.renderer.rules.marp_vscode_style = (token, i) =>
     `<style id="marp-vscode-style">${token[i].content}</style>`
+
+  // Override default highlighter to fix wrong background color
+  const originalHighlighter = md.options.highlight
+
+  md.set({
+    highlight: function marpHighlighter(...args) {
+      return (
+        (md[marpVscodeEnabled] && marp.highlighter(...args)) ||
+        originalHighlighter(...args)
+      )
+    },
+  })
 
   return md
 }
