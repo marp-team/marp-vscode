bdd3f172e12909920135f39d461a635165ef778a
Yuki Hattori <yukihattori1116@gmail.com>
2019-04-08 02:09:36 +0900

    Cache calculated Marp option

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,7 @@
-import { Marp } from '@marp-team/marp-core'
+import { Marp, MarpOptions } from '@marp-team/marp-core'
 import { ExtensionContext, commands, workspace } from 'vscode'
+
+let cachedMarpOption: MarpOptions | undefined
 
 const frontMatterRegex = /^-{3,}\s*([^]*?)^\s*-{3}/m
 const marpDirectiveRegex = /^marp\s*:\s*true\s*$/m
@@ -8,13 +10,42 @@ const marpVscode = Symbol('marp-vscode')
 const shouldRefreshConfs = [
   'markdown.marp.breaks',
   'markdown.marp.enableHtml',
-  // 'markdown.preview.breaks',
+  'markdown.preview.breaks',
   'window.zoomLevel',
 ]
 
 const detectMarpFromFrontMatter = (markdown: string): boolean => {
   const m = markdown.match(frontMatterRegex)
   return !!(m && m.index === 0 && marpDirectiveRegex.exec(m[0].trim()))
+}
+
+const marpOption = (): MarpOptions => {
+  if (!cachedMarpOption) {
+    const breaks = (() => {
+      switch (marpConfiguration().get<string>('breaks')) {
+        case 'off':
+          return false
+        case 'inherit':
+          return (
+            workspace
+              .getConfiguration('markdown.preview')
+              .get<boolean>('breaks') || false
+          )
+        default:
+          return true
+      }
+    })()
+
+    const zoom =
+      workspace.getConfiguration('window').get<number>('zoomLevel') || 0
+
+    cachedMarpOption = {
+      container: { tag: 'div', id: 'marp-vscode', 'data-zoom': 1.2 ** zoom },
+      html: marpConfiguration().get<boolean>('enableHtml') || undefined,
+      markdown: { breaks },
+    }
+  }
+  return cachedMarpOption
 }
 
 export function extendMarkdownIt(md: any) {
@@ -24,29 +55,7 @@ export function extendMarkdownIt(md: any) {
   md.parse = (markdown: string, env: any) => {
     // Generate tokens by Marp if enabled
     if (detectMarpFromFrontMatter(markdown)) {
-      const breaks = (() => {
-        const marpBreaks = marpConfiguration().get<string>('breaks')
-
-        if (marpBreaks === 'off') return false
-        if (marpBreaks === 'inherit') {
-          return (
-            workspace
-              .getConfiguration('markdown.preview')
-              .get<boolean>('breaks') || false
-          )
-        }
-        return true
-      })()
-
-      const zoom =
-        workspace.getConfiguration('window').get<number>('zoomLevel') || 0
-
-      md[marpVscode] = new Marp({
-        container: { tag: 'div', id: 'marp-vscode', 'data-zoom': 1.2 ** zoom },
-        html: marpConfiguration().get<boolean>('enableHtml') || undefined,
-        markdown: { breaks },
-      })
-
+      md[marpVscode] = new Marp(marpOption())
       return md[marpVscode].markdown.parse(markdown, env)
     }
 
@@ -76,6 +85,7 @@ export const activate = ({ subscriptions }: ExtensionContext) => {
   subscriptions.push(
     workspace.onDidChangeConfiguration(e => {
       if (shouldRefreshConfs.some(c => e.affectsConfiguration(c))) {
+        cachedMarpOption = undefined
         commands.executeCommand('markdown.preview.refresh')
       }
     })
