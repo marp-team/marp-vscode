8de38352f2843c5007cd282d94315adb7b52b35c
Yuki Hattori <yuki.hattori@speee.jp>
2019-06-13 10:04:22 +0900

    Enable better line sync only when disabled polyfill

--- src/plugins/line-number.ts
+++ src/plugins/line-number.ts
@@ -1,6 +1,20 @@
+import { isRequiredPolyfill } from '../option'
+
+// https://github.com/microsoft/vscode/blob/master/extensions/markdown-language-features/src/markdownEngine.ts
+const rules = [
+  'paragraph_open',
+  'heading_open',
+  'image',
+  'code_block',
+  'fence',
+  'blockquote_open',
+  'list_item_open',
+]
+
 export default function marpVSCodeLineNumber(md) {
   const { marpit_inline_svg_open } = md.renderer.rules
 
+  // Enable line sync by per slides
   md.renderer.rules.marpit_inline_svg_open = (tokens, idx, opts, env, self) => {
     const slide = tokens
       .slice(idx + 1)
@@ -13,5 +27,25 @@ export default function marpVSCodeLineNumber(md) {
 
     const renderer = marpit_inline_svg_open || self.renderToken
     return renderer.call(self, tokens, idx, opts, env, self)
+  }
+
+  // Enables better line sync only when disabled poiyfill 
+  // (There are wrong DOM positions if enabled polyfill)
+  if (!isRequiredPolyfill) {
+    for (const rule of rules) {
+      const original = md.renderer.rules[rule]
+
+      md.renderer.rules[rule] = (tokens, idx, options, env, self) => {
+        const token = tokens[idx]
+
+        if (token.map && token.map.length) {
+          token.attrJoin('class', 'code-line')
+          token.attrSet('data-line', token.map[0])
+        }
+
+        const renderer = original || self.renderToken
+        return renderer.call(self, tokens, idx, options, env, self)
+      }
+    }
   }
 }
