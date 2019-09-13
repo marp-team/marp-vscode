cc8a7a624ff1f8bc526f7f711c22cadd68a77afe
Yuki Hattori <yuki.hattori@speee.jp>
2019-09-13 18:05:47 +0900

    Warn deprecated $ prefix in frontmatter directives

--- src/diagnostics.ts
+++ src/diagnostics.ts
@@ -1,12 +1,15 @@
 import {
   Diagnostic,
+  DiagnosticSeverity,
   Disposable,
+  Range,
   TextDocument,
   languages,
   window,
   workspace,
 } from 'vscode'
-import { detectMarpDocument } from './utils'
+import yaml from 'yaml'
+import { detectMarpDocument, frontMatterRegex } from './utils'
 
 export const collection = languages.createDiagnosticCollection('marp-vscode')
 
@@ -38,7 +41,7 @@ function warnDeprecatedDollarPrefix(
   doc: TextDocument,
   diagnostics: Diagnostic[]
 ) {
-  const text = doc.getText()
+  const markdown = doc.getText()
   const warnDirectives = [
     // Marpit
     '$headingDivider',
@@ -55,7 +58,36 @@ function warnDeprecatedDollarPrefix(
     '$url',
   ]
 
-  // TODO: Add diagnostics when detected global directives with dollar prefix
+  const detectDirectives = (text: string, offset: number = 0) => {
+    const { contents } = yaml.parseDocument(text, { schema: 'failsafe' })
+    if (!(contents && contents['items'])) return
+
+    for (const item of contents['items']) {
+      if (item.type === 'PAIR' && warnDirectives.includes(item.key.value)) {
+        const name = item.key.value.slice(1)
+        const [start, end] = item.key.range
+
+        diagnostics.push(
+          new Diagnostic(
+            new Range(
+              doc.positionAt(start + offset),
+              doc.positionAt(end + offset)
+            ),
+            `Dollar prefix support for ${name} global directive is deprecated. Remove "$" to fix.`,
+            DiagnosticSeverity.Warning
+          )
+        )
+      }
+    }
+  }
+
+  // Front-matter
+  const fmMatched = markdown.match(frontMatterRegex)
+  if (fmMatched && fmMatched.index === 0) {
+    detectDirectives(fmMatched[2], fmMatched[1].length)
+  }
+
+  // TODO: Parse directives in HTML comments
 }
 
 export default subscribe
--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,12 @@
 import { TextDocument, workspace } from 'vscode'
 
-export const frontMatterRegex = /^-{3,}\s*$\n([\s\S]*?)^\s*-{3}/m
+export const frontMatterRegex = /^(-{3,}\s*$\n)([\s\S]*?)^(\s*-{3})/m
 
 export const marpDirectiveRegex = /^(marp\s*: +)(.*)\s*$/m
 
 export const detectFrontMatter = (markdown: string): string | undefined => {
   const m = markdown.match(frontMatterRegex)
-  return m && m.index === 0 ? m[1] : undefined
+  return m && m.index === 0 ? m[2] : undefined
 }
 
 export const detectMarpDocument = (doc: TextDocument): boolean =>
