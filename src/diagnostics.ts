bc99440ddf1621adac7c6aba3c4bc1bdf94e55df
Yuki Hattori <yukihattori1116@gmail.com>
2019-09-13 23:42:12 +0900

    Warn deprecated $ prefix in HTML comments

--- src/diagnostics.ts
+++ src/diagnostics.ts
@@ -1,3 +1,4 @@
+import lodashDebounce from 'lodash.debounce'
 import rehypeParse from 'rehype-parse'
 import remarkParse from 'remark-parse'
 import unified from 'unified'
@@ -15,15 +16,24 @@ import {
 import yaml from 'yaml'
 import { detectMarpDocument, frontMatterRegex } from './utils'
 
+const parseHtml = unified().use(rehypeParse).parse
+const parseMd = unified().use(remarkParse, { commonmark: true }).parse
+const parseYaml = (yamlBody: string) =>
+  yaml.parseDocument(yamlBody, { schema: 'failsafe' })
+
 export const collection = languages.createDiagnosticCollection('marp-vscode')
+
+const setDiagnostics = lodashDebounce((doc: TextDocument) => {
+  const diagnostics: Diagnostic[] = []
+
+  warnDeprecatedDollarPrefix(doc, diagnostics)
+
+  collection.set(doc.uri, diagnostics)
+}, 500)
 
 export function refresh(doc: TextDocument) {
   if (detectMarpDocument(doc)) {
-    const diagnostics: Diagnostic[] = []
-
-    warnDeprecatedDollarPrefix(doc, diagnostics)
-
-    collection.set(doc.uri, diagnostics)
+    setDiagnostics(doc)
   } else {
     collection.delete(doc.uri)
   }
@@ -65,24 +75,25 @@ function warnDeprecatedDollarPrefix(
   ]
 
   const detectDirectives = (text: string, offset: number = 0) => {
-    const { contents } = yaml.parseDocument(text, { schema: 'failsafe' })
-    if (!(contents && contents['items'])) return
+    const { contents, errors } = parseYaml(text)
 
-    for (const item of contents['items']) {
-      if (item.type === 'PAIR' && warnDirectives.includes(item.key.value)) {
-        const name = item.key.value.slice(1)
-        const [start, end] = item.key.range
+    if (errors.length === 0 && contents && contents['items']) {
+      for (const item of contents['items']) {
+        if (item.type === 'PAIR' && warnDirectives.includes(item.key.value)) {
+          const name = item.key.value.slice(1)
+          const [start, end] = item.key.range
 
-        diagnostics.push(
-          new Diagnostic(
-            new Range(
-              doc.positionAt(start + offset),
-              doc.positionAt(end + offset)
-            ),
-            `Dollar prefix support for ${name} global directive is deprecated. Remove "$" to fix.`,
-            DiagnosticSeverity.Warning
+          diagnostics.push(
+            new Diagnostic(
+              new Range(
+                doc.positionAt(start + offset),
+                doc.positionAt(end + offset)
+              ),
+              `Dollar prefix support for ${name} global directive is deprecated. Remove "$" to fix.`,
+              DiagnosticSeverity.Warning
+            )
           )
-        )
+        }
       }
     }
   }
@@ -99,12 +110,17 @@ function warnDeprecatedDollarPrefix(
   }
 
   // HTML comments
-  const mdParser = unified().use(remarkParse, { commonmark: true })
-  const htmlParser = unified().use(rehypeParse)
-
-  visit(mdParser.parse(markdown), 'html', (n: any) => {
-    console.log(htmlParser.parse(n.value))
-  })
+  visit(parseMd(markdown), 'html', (n: any) =>
+    visit(parseHtml(n.value), 'comment', (c: any) =>
+      detectDirectives(
+        c.value.trim(),
+        index +
+          n.position.start.offset +
+          4 +
+          (c.value.length - c.value.trimLeft().length)
+      )
+    )
+  )
 }
 
 export default subscribe
