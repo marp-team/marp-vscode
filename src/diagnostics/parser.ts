0695d81d142f735f75829ea5305ccbac33d378d3
Yuki Hattori <yukihattori1116@gmail.com>
2021-05-01 07:19:09 +0900

    Upgrade dependent packages to the latest version

--- src/diagnostics/parser.ts
+++ src/diagnostics/parser.ts
@@ -4,6 +4,6 @@ import unified from 'unified'
 import yaml from 'yaml'
 
 export const parseHtml = unified().use(rehypeParse).parse
-export const parseMd = unified().use(remarkParse, { commonmark: true }).parse
+export const parseMd = unified().use(remarkParse).parse
 export const parseYaml = (yamlBody: string) =>
   yaml.parseDocument(yamlBody, { schema: 'failsafe' })
