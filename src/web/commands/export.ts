ea05bee0b0d3ac605e7fd7875324bd21ea8775fb
Yuki Hattori <yukihattori1116@gmail.com>
2021-08-31 06:34:00 +0900

    Update error message for export command on the web

--- src/web/commands/export.ts
+++ src/web/commands/export.ts
@@ -3,5 +3,7 @@ import { window } from 'vscode'
 export const command = 'markdown.marp.export'
 
 export default function exportCommand() {
-  window.showErrorMessage('Export command cannot run on the browser.')
+  window.showErrorMessage(
+    'Export command cannot run by an extension for the Web. Please consider to use the extension in full-featured VS Code such as GitHub Codespaces or locally installed VS Code.'
+  )
 }
